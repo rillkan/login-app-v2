a96cfe5f22836ecee2257c07752f75f699652e94
Reagan Kan <mjkan96@gmail.com>
2024-08-02 15:33:17 +0800

    add button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
-import { Container } from "react-bootstrap"
+import { Button, Container } from "react-bootstrap"
+import { useNavigate } from "react-router-dom"
 
 export default function Home() {
+
+  const navigate = useNavigate();
+
+  const goTologin = () => {
+    navigate("/login")
+  }
+
   return (
     <Container>
       <h1 className="my-3">Welcome Home</h1>
+      <Button onClick={goTologin}>Login Page</Button>
     </Container>
   )
 }
